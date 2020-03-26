2caa3f5550587e51f2908a707eba9c36b22b003f
Guillaume Bury <guillaume.bury@gmail.com>
2020-03-26 14:25:47 +0100

    Better bigarray primitives + liveness cache update
    Change the status of bigarray primitives:
    - bigarray_load is now a binary primitive
    - bigarray_store is now a ternary primitive
    
    Translation from multi-dimension addressing to linear addressing is
    done in the primitive translation, so that the bigarray primitives take
    a single linear offset.
    
    Adidtionally the unknwon variant for bigarray kind and layout in
    flambda2 are removed because theyr are translated into c-calls in
    prepare_lambda.
    
    Cmm code generation has been adapted.
    
    Better cache: Use subset comparison, as the functions are monotone

--- lambda/lambda.ml
+++ lambda/lambda.ml
@@ -251,6 +251,14 @@ let primitive_can_raise = function
   | Pbigstring_set_64 false
   | Pdivbint { is_safe = Safe; _ }
   | Pmodbint { is_safe = Safe; _ }
+  | Pbigarrayref (false, _, _, _)
+  | Pbigarrayset (false, _, _, _)
+  (* These bigarray primitives are translated into c-calls which may
+     raise even if the unsafe flag is true *)
+  | Pbigarrayref (_, _, Pbigarray_unknown, _)
+  | Pbigarrayset (_, _, Pbigarray_unknown, _)
+  | Pbigarrayref (_, _, _, Pbigarray_unknown_layout)
+  | Pbigarrayset (_, _, _, Pbigarray_unknown_layout)
     (* CR mshinwell: some more need to go here too *)
     -> true
   | Pidentity
