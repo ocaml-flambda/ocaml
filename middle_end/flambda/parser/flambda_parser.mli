1eab1a52ad6ef46745fc03b07915d2b1b892f1ec
Luke Maurer <luke.v.maurer@gmail.com>
2020-08-03 17:08:26 +0100

    New Flambda concrete syntax

--- middle_end/flambda/parser/flambda_parser.mli
+++ middle_end/flambda/parser/flambda_parser.mli
@@ -2,55 +2,70 @@
 (* The type of tokens. *)
 
 type token = 
+  | WITH
+  | WHERE
+  | VAL
+  | UNTAG_IMM
   | UNREACHABLE
+  | UNIT
   | UNDERSCORE
   | UIDENT of (string)
-  | TAG
+  | TUPLED
+  | SYMBOL
   | SWITCH
   | STUB
   | STAR
+  | SET_OF_CLOSURES
   | SEMICOLON
+  | SELECT_CLOSURE
   | RPAREN
-  | ROOT
   | REC
-  | RBRACKET
   | RBRACE
+  | PROJECT_VAR
   | PLUSDOT
   | PLUS
+  | PIPE
+  | PHYS_NE
+  | PHYS_EQ
   | OPAQUE
-  | MUT
+  | NOALLOC
+  | NEWER_VERSION_OF
+  | NATIVEINT
   | MINUSGREATER
   | MINUSDOT
   | MINUS
   | LPAREN
   | LIDENT of (string)
-  | LETK
   | LET
-  | LBRACKET
   | LBRACE
-  | IS_INT
+  | INT64
+  | INT32
   | INT of (string * char option)
   | IN
+  | IMM
   | HCF
-  | GET_FIELD
+  | FLOAT_KIND
   | FLOAT of (string * char option)
+  | FABRICATED
   | EXN
+  | ERROR
   | EQUAL
   | EOF
-  | EFFECT
+  | END
   | DOT
-  | DEF
+  | DONE
+  | DIRECT
+  | DELETED
   | CONT
   | COMMA
-  | COLONEQUAL
   | COLON
   | CODE
   | CLOSURE
   | CCALL
   | BLOCK
-  | BANG
-  | AROBASE
+  | AT
   | APPLY
+  | ANDWHERE
   | AND
 
 (* This exception is raised by the monolithic API functions. *)
@@ -59,4 +74,21 @@ exception Error
 
 (* The monolithic API. *)
 
-val program: (Lexing.lexbuf -> token) -> Lexing.lexbuf -> (Fexpr.program)
+val flambda_unit: (Lexing.lexbuf -> token) -> Lexing.lexbuf -> (Fexpr.flambda_unit)
+
+module MenhirInterpreter : sig
+  
+  (* The incremental API. *)
+  
+  include CamlinternalMenhirLib.IncrementalEngine.INCREMENTAL_ENGINE
+    with type token = token
+  
+end
+
+(* The entry point(s) to the incremental API. *)
+
+module Incremental : sig
+  
+  val flambda_unit: Lexing.position -> (Fexpr.flambda_unit) MenhirInterpreter.checkpoint
+  
+end
